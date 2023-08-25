2e9007353ae315c0564f50ede9260bddece99f50
Ayush Saha <71895904+Ayushsaha103@users.noreply.github.com>
2023-08-24 22:40:23 -0700

    Create BayesianNeuralNetwork.py

---
+++ BayesianNeuralNetwork.py
@@ -0,0 +1,3 @@
+# Bayesian Neural Network (implemented via Tensorflow)
+# Use this link to access the Colab Notebook:
+# https://drive.google.com/file/d/1IUnonqHRSvQQDp4MYnQ4Mqc7_HIkL_Eo/view?usp=sharing
